8df4dc3b197649d1caa81bbc896790d94f8b5b0a
Stupdi Go <mukund@ayyalasomayajula.net>
2025-01-22 20:38:38 -0600

    2.0 out

--- worldguessr_cheats/content.js
+++ worldguessr_cheats/content.js
@@ -13,8 +13,19 @@ button.style.color = "white";
 button.style.border = "none";
 button.style.borderRadius = "5px";
 button.style.zIndex = "10000";
-
-// Add click event to the button
+let current_iframe_id = "worldguessr-com_728x90";
+let display = document.createElement("div");
+// Add click event to the Button
+function createRandomString(length) {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  const randomArray = new Uint8Array(length);
+  crypto.getRandomValues(randomArray);
+  randomArray.forEach((number) => {
+    result += chars[number % chars.length];
+  });
+  return result;
+}
 button.addEventListener("click", () => {
   console.log("Button clicked!");
   let iframe = document.querySelector('[title="Street View Embed"]');
@@ -29,14 +40,29 @@ button.addEventListener("click", () => {
       longitude = parseFloat(unparsed[i].split("=")[1]);
     }
   }
-  let display = document.getElementById("worldguessr-com_728x90");
   
-  let html = `<iframe id="map-canvas" class="map_part" width="600"  height="450"  frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%&amp;height=100%&amp;hl=en&amp;q=${latitude},${longitude}&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">Powered by <a href="https://embedgooglemaps.com">embed google maps html</a> and <a href="https://starburstnotongamstop.org/">starburst not on gamstop</a></iframe>`
+  let character_num = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+  current_iframe_id = createRandomString(character_num);
+  display.id = current_iframe_id;
+
+  
+  let html = `<iframe class="map_part" width="600"  height="450"  frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%&amp;height=100%&amp;hl=en&amp;q=${latitude},${longitude}&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">Powered by <a href="https://embedgooglemaps.com">embed google maps html</a> and <a href="https://starburstnotongamstop.org/">starburst not on gamstop</a></iframe>`
   display.style.height="450px";
   display.style.width="600px";
+  display.style.position = "fixed";
+  display.style.top = "20px"; // Adjust the vertical position from the top (20px from the top)
+  display.style.left = "50%"; // Center horizontally
+  display.style.transform = "translateX(-50%)";
+  display.style.zIndex = "100000";
   display.innerHTML = html;
+  display.style.zoom = "0.75";
   console.log("Latitude",latitude);
   console.log("Longitude",longitude);
+  if (document.body.contains(display)) {
+    document.body.removeChild(display);
+  }
+
+  document.body.appendChild(display);
 });
 
 // Append the button to the page
