cfab6a4aae979a2f0157308483971b1b075899e0
Rohan <rohanjain0725@gmail.com>
2025-05-01 14:24:50 -0500

    Update content.js

--- worldguessr_cheats/content.js
+++ worldguessr_cheats/content.js
@@ -1,7 +1,7 @@
 // Create a Button
 console.log("WORKING");
 const button = document.createElement("button");
-button.textContent = "Reload Location";
+button.textContent = "Find Location";
 button.style.position = "fixed";
 button.style.bottom = "50%";
 button.style.left = "20px";
@@ -15,17 +15,7 @@ button.style.borderRadius = "5px";
 button.style.zIndex = "10000";
 let current_iframe_id = "worldguessr-com_728x90";
 let display = document.createElement("div");
-// Add click event to the Button
-function createRandomString(length) {
-  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let result = "";
-  const randomArray = new Uint8Array(length);
-  crypto.getRandomValues(randomArray);
-  randomArray.forEach((number) => {
-    result += chars[number % chars.length];
-  });
-  return result;
-}
+
 button.addEventListener("click", () => {
   console.log("Button clicked!");
   let iframe = document.querySelector('[title="Street View Embed"]');
@@ -40,30 +30,12 @@ button.addEventListener("click", () => {
       longitude = parseFloat(unparsed[i].split("=")[1]);
     }
   }
-  
-  let character_num = Math.floor(Math.random() * (10 - 1 + 1) + 1);
-  current_iframe_id = createRandomString(character_num);
-  display.id = current_iframe_id;
 
+  let link = `https://www.google.com/maps/search/${latitude}+${longitude}/`;
+  openPopup(link);
   
-  let html = `<iframe class="map_part" width="600"  height="450"  frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%&amp;height=100%&amp;hl=en&amp;q=${latitude},${longitude}&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">Powered by <a href="https://embedgooglemaps.com">embed google maps html</a> and <a href="https://starburstnotongamstop.org/">starburst not on gamstop</a></iframe>`
-  display.style.height="450px";
-  display.style.width="600px";
-  display.style.position = "fixed";
-  display.style.top = "20px"; // Adjust the vertical position from the top (20px from the top)
-  display.style.left = "50%"; // Center horizontally
-  display.style.transform = "translateX(-50%)";
-  display.style.zIndex = "100000";
-  display.innerHTML = html;
-  display.style.zoom = "0.75";
-  console.log("Latitude",latitude);
-  console.log("Longitude",longitude);
-  if (document.body.contains(display)) {
-    document.body.removeChild(display);
-  }
-
-  document.body.appendChild(display);
 });
 
-// Append the button to the page
-document.body.appendChild(button);
+function openPopup(link) {
+            window.open(link, "popupWindow", "width=600,height=400,scrollbars=yes");
+}
